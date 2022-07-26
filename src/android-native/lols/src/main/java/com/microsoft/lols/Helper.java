c03030fc2467a736cdf26ba7f7a88ca4a2e1e53f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-07-26 15:18:27 -0500

    Create Java project

---
+++ src/android-native/lols/src/main/java/com/microsoft/lols/Helper.java
@@ -0,0 +1,31 @@
+package com.microsoft.lols;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Helper {
+	public static TextView createTextView (Context context, int color, float rotation, int x, int y)
+	{
+		TextView textView = new TextView(context);
+		textView.setText("lol?");
+		textView.setTextColor(color);
+		textView.setRotation(rotation);
+
+		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		layoutParams.leftMargin = x;
+		layoutParams.topMargin = y;
+		textView.setLayoutParams(layoutParams);
+
+		return textView;
+	}
+
+	public static void add(RelativeLayout layout, TextView textView)
+	{
+		int childCount = layout.getChildCount();
+		if (childCount > 500)
+			layout.removeViewAt(childCount - 2);
+		layout.addView(textView);
+	}
+}
