7194747a0d4ad33fd24b96366d4fb03c5eab122c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-07-26 14:56:21 -0500

    LOL/s on Android

--- src/android/MainActivity.cs
+++ src/android/MainActivity.cs
@@ -1,13 +1,81 @@
+using Android.Graphics;
+using Stopwatch = System.Diagnostics.Stopwatch;
+
 namespace lols;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
 {
-    protected override void OnCreate(Bundle? savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
+	const int Max = 500;
+	int count = 0;
+	readonly System.Timers.Timer timer = new System.Timers.Timer(500);
+	readonly Stopwatch stopwatch = new Stopwatch();
+	TextView lols;
+	RelativeLayout layout;
 
-        // Set our view from the "main" layout resource
-        SetContentView(Resource.Layout.activity_main);
-    }
+	protected override void OnCreate(Bundle? savedInstanceState)
+	{
+		base.OnCreate(savedInstanceState);
+
+		SetContentView(Resource.Layout.activity_main);
+		lols = RequireViewById<TextView>(Resource.Id.lols);
+		layout = RequireViewById<RelativeLayout>(Resource.Id.layout);
+
+		timer.Elapsed += OnTimer;
+
+		stopwatch.Start();
+		timer.Start();
+		_ = Task.Factory.StartNew(RunTest, TaskCreationOptions.LongRunning);
+	}
+
+	void OnTimer(object? sender, System.Timers.ElapsedEventArgs e)
+	{
+		double avg = count / stopwatch.Elapsed.TotalSeconds;
+		string text = "LOL/s: " + avg;
+		RunOnUiThread(() => UpdateText(text));
+	}
+
+	void UpdateText(string text) => lols!.Text = text;
+
+	async void RunTest()
+	{
+		var random = Random.Shared;
+		int width = layout.Width;
+		int height = layout.Height;
+
+		//TODO: something better?
+		while (width == 0 || height == 0)
+		{
+			width = layout.Width;
+			height = layout.Height;
+		}
+
+		while (count < 5000)
+		{
+			var label = new TextView(this)
+			{
+				Text = "lol?",
+				Rotation = random.NextSingle() * 360
+			};
+			label.SetTextColor(new Color(random.Next(byte.MaxValue), random.Next(byte.MaxValue), random.Next(byte.MaxValue)));
+			label.LayoutParameters = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent)
+			{
+				LeftMargin = random.Next(width),
+				TopMargin = random.Next(height),
+			};
+			RunOnUiThread(() =>
+			{
+				var childCount = layout.ChildCount;
+				if (childCount > Max)
+					layout.RemoveViewAt (childCount - 2); // - 1 is the label on top
+				layout.AddView (label, 0);
+				
+				count++;
+			});
+			await Task.Delay(1);
+		}
+
+		stopwatch.Stop();
+		timer.Stop();
+	}
 }
